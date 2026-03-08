9aeab20d60996385340587d3dc705ebeab133237
hpan290504 <hpan290504@gmail.com>
2026-03-08 13:22:39 +0700

    version9.6

---
+++ api/debug-env.js
@@ -0,0 +1,9 @@
+module.exports = (req, res) => {
+    res.status(200).json({
+        status: "alive",
+        node: process.version,
+        env: process.env.NODE_ENV,
+        hasDb: !!process.env.DATABASE_URL,
+        hasJwtSecret: !!process.env.JWT_SECRET
+    });
+};
