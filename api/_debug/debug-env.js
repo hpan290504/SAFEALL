901b66ae19e6d2a69a2c699cca6e6aa9ff3e2bc7
hpan290504 <hpan290504@gmail.com>
2026-03-09 14:48:12 +0700

    ver.26.0 - Fixed Vercel deployment by removing redundant API functions (limit 12)

--- api/_debug/debug-env.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        status: "alive",
-        node: process.version,
-        env: process.env.NODE_ENV,
-        hasDb: !!process.env.DATABASE_URL,
-        hasJwtSecret: !!process.env.JWT_SECRET
-    });
-};
